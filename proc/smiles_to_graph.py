4a86945e475e63c478f383c6677c6025944df750
Adam <adam.perrott@protonmail.com>
2026-03-04 12:27:08 +0000

    feat: add 3d structural properties into node features

--- proc/smiles_to_graph.py
+++ proc/smiles_to_graph.py
@@ -1,10 +1,11 @@
 from rdkit import Chem
-from rdkit.Chem import Mol, Atom, Bond
+from rdkit.Chem import Mol, Atom, Bond, rdDistGeom, rdForceFieldHelpers, rdMolAlign, Conformer, rdPartialCharges
 import torch
 from torch import Tensor
 from torch_geometric.data import Data # type: ignore
 import pandas as pd
-from typing import Optional
+from typing import Optional, Sequence
+import numpy as np
 
 
 COMMON_ATOMS = ['H','C','N','O','F','P','S','Cl','Br','I']
@@ -17,122 +18,183 @@ HYBRIDIZATION_TYPES = [
     Chem.rdchem.HybridizationType.SP3D2,
 ]
 
+class MoleculeRepresentation:
+    def __init__(self, smiles: str, inh_pow: float):
+        self.smiles = smiles
+        self.molecule = Chem.MolFromSmiles(smiles)
+        self.inh_pow = inh_pow
 
-def one_hot(value, choices):
-    return [int(value == c) for c in choices]
-
-
-def atom_features(atom: Atom) -> Tensor:
-    """Creates a pytorch tensor that is the node-features for a specific atom.
-
-    Args:
-        atom (Atom): The atom(node) in the molecule to get the data for.
-
-    Returns:
-        Tensor: Has dimension 23 and contains  things like atomic number, degree, formal charge, chirality
-            and whether something is aromatic. These are returned as floats in the tensor.
-    """
-
-    symbol = atom.GetSymbol()
-    features = []
-
-    # One-hot atomic symbol (10 dim)
-    features += one_hot(symbol, COMMON_ATOMS)
+    @staticmethod
+    def one_hot(value, choices):
+        return [int(value == c) for c in choices]
     
-    # Atomic number (scaled) for uncommon atoms (1 dim)
-    features.append(atom.GetAtomicNum() / 100.0)
+    def atom_features(self, atom: Atom, conf: Conformer) -> Tensor:
+        """Creates a pytorch tensor that is the node-features for a specific atom.
 
-    # Degree (1 dim)
-    features.append(atom.GetTotalDegree())
+        Args:
+            atom (Atom): The atom(node) in the molecule to get the data for.
 
-    # Formal Charge (1 dim)
-    features.append(atom.GetFormalCharge())
+        Returns:
+            Tensor: Has dimension 23 and contains  things like atomic number, degree, formal charge, chirality
+                and whether something is aromatic. These are returned as floats in the tensor.
+        """
+        rdPartialCharges.ComputeGasteigerCharges(self.molecule)
+        symbol = atom.GetSymbol()
+        features = []
 
-    # Hydridization (5 dim)
-    features += one_hot(atom.GetHybridization, HYBRIDIZATION_TYPES)
+        # One-hot atomic symbol (10 dim)
+        features += self.one_hot(symbol, COMMON_ATOMS)
+        
+        # Atomic number (scaled) for uncommon atoms (1 dim)
+        features.append(atom.GetAtomicNum() / 100.0)
 
-    # Aromaticity (1 dim)
-    features.append(int(atom.GetIsAromatic()))
+        # Degree (1 dim)
+        features.append(atom.GetTotalDegree())
 
-    # Number of Hydrogens (1 dim)
-    features.append(atom.GetTotalNumHs())
+        # Formal Charge (1 dim)
+        features.append(atom.GetFormalCharge())
 
-    # In Ring (1 dim)
-    features.append(int(atom.IsInRing()))
+        # Hydridization (5 dim)
+        features += self.one_hot(atom.GetHybridization, HYBRIDIZATION_TYPES)
 
-    # Chirality (2 dim)
-    chiral_tag = atom.GetChiralTag()
-    features.append(int(chiral_tag == Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW))
-    features.append(int(chiral_tag == Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW))
+        # Aromaticity (1 dim)
+        features.append(int(atom.GetIsAromatic()))
 
-    # Atomic Mass (1 dim) (scaled)
-    features.append(atom.GetMass() * 0.01)
+        # Number of Hydrogens (1 dim)
+        features.append(atom.GetTotalNumHs())
 
-    return torch.tensor(features, dtype=torch.float)
+        # In Ring (1 dim)
+        features.append(int(atom.IsInRing()))
 
-def bond_features(bond: Bond) -> Tensor:
-    """Creates a pytorch tensor that is the edge features for a specific bond.
+        # Chirality (2 dim)
+        chiral_tag = atom.GetChiralTag()
+        features.append(int(chiral_tag == Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW))
+        features.append(int(chiral_tag == Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW))
 
-    Args:
-        bond (Bond): The bond(edge) in the molecule to get the data for.
+        # Atomic Mass (1 dim) (scaled)
+        features.append(atom.GetMass() * 0.01)
 
-    Returns:
-        Tensor: Has dimension 4 and is a one-hot encoding which tells the model whether
-            the bond is single, double, triple or aromatic.
-    """
-    return torch.tensor([
-        bond.GetBondType() == Chem.rdchem.BondType.SINGLE,
-        bond.GetBondType() == Chem.rdchem.BondType.DOUBLE,
-        bond.GetBondType() == Chem.rdchem.BondType.TRIPLE,
-        bond.GetBondType() == Chem.rdchem.BondType.AROMATIC,
-        bond.GetIsConjugated(),
-        bond.IsInRing(),
-        ], dtype=torch.float)
+        # Partial Charge (1 dim)
+        charge = float(atom.GetProp("_GasteigerCharge"))
+        features.append(charge)
 
-def mol_to_graph(smiles: str, inh_pow: float) -> Optional[Data]:
-    """Creates a graph with the necessary embeddings from a given SMILES string.
+        # Positions (3 dim)
+        pos = conf.GetAtomPosition(atom.GetIdx())
+        features += [pos.x, pos.y, pos.z]
 
-    Args:
-        smiles (str): The molecular structure given in SMILES format.
-        inh_pow (float): The inhibition power for the given molecule, this is the target
-            property.
+        return torch.tensor(features, dtype=torch.float)
 
-    Returns:
-        Data: A torch-geometric Data type which is a graph containing all the structural and
-            embedding information. If the SMILES string is unreadable or corrupt then None is
-            returned.
-    """
-    mol: Mol = Chem.MolFromSmiles(smiles)
-    if mol is None:
-        return None
+    def bond_features(self, bond: Bond) -> Tensor:
+        """Creates a pytorch tensor that is the edge features for a specific bond.
 
-    # Node features
-    x = torch.stack([atom_features(atom) for atom in mol.GetAtoms()])
+        Args:
+            bond (Bond): The bond(edge) in the molecule to get the data for.
 
-    # Edges
-    edge_index = []
-    edge_attr = []
-
-    for bond in mol.GetBonds():
-        i = bond.GetBeginAtomIdx()
-        j = bond.GetEndAtomIdx()
-
-        # Undirected graph → add both directions
-        edge_index.append([i, j])
-        edge_index.append([j, i])
-
-        bf = bond_features(bond)
-        edge_attr.append(bf)
-        edge_attr.append(bf)
+        Returns:
+            Tensor: Has dimension 4 and is a one-hot encoding which tells the model whether
+                the bond is single, double, triple or aromatic.
+        """
+        return torch.tensor([
+            bond.GetBondType() == Chem.rdchem.BondType.SINGLE,
+            bond.GetBondType() == Chem.rdchem.BondType.DOUBLE,
+            bond.GetBondType() == Chem.rdchem.BondType.TRIPLE,
+            bond.GetBondType() == Chem.rdchem.BondType.AROMATIC,
+            bond.GetIsConjugated(),
+            bond.IsInRing(),
+            ], dtype=torch.float)
     
-    # Turns a list of tuples into a tensor and then changes the shape and memory type.
-    edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
-    # Combines a list of tensors into one big tensor of the required shape.
-    edge_attr = torch.stack(edge_attr)
+    @staticmethod
+    def pick_central_conformer(molecule: Mol, conf_ids: Sequence[int]) -> int:
+        rmsd_matrix = []
+        for i in conf_ids:
+            row = []
+            for j in conf_ids:
+                rms = rdMolAlign.GetBestRMS(molecule, molecule, prbId=i, refId=j)
+                row.append(rms)
+            rmsd_matrix.append(row)
+        
+        rmsd_matrix = np.array(rmsd_matrix)
+        mean_rmsd = rmsd_matrix.mean(axis=1)
+        return conf_ids[int(np.argmin(mean_rmsd))]
+    
+    
+    def find_best_conformer(self) -> Optional[Conformer]:
+        if self.molecule is None:
+            return None
+        self.molecule_3d = Chem.AddHs(self.molecule)
+        
+        params = rdDistGeom.ETKDGv3()
+        params.numThreads = 0
+        params.pruneRmsThresh = 0.1
+        conf_ids = rdDistGeom.EmbedMultipleConfs(self.molecule_3d, numConfs=50, params=params)
 
-    y = torch.tensor([inh_pow], dtype=torch.float)
+        if not rdForceFieldHelpers.MMFFHasAllMoleculeParams(self.molecule_3d):
+            print("UFF does not support this molecule. Skipping minimization and using approximate geometry.")
+            best_id = self.pick_central_conformer(self.molecule_3d, conf_ids)
+            return self.molecule_3d.GetConformer(best_id)
+        
+        mp = rdForceFieldHelpers.MMFFGetMoleculeProperties(self.molecule_3d)
+        energies = []
+        for cid in conf_ids:
+            ff = rdForceFieldHelpers.MMFFGetMoleculeForceField(
+                self.molecule_3d,
+                mp,
+                confId=cid
+            )
+            ff.Minimize()
+            energies.append((cid, ff.CalcEnergy()))
+        
 
-    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+
+        energies.sort(key=lambda x: x[1])
+        best_conf = energies[0][0]
+        return self.molecule_3d.GetConformer(best_conf)
+
+    def mol_to_graph(self) -> Optional[Data]:
+        """Creates a graph with the necessary embeddings from a given SMILES string.
+
+        Args:
+            smiles (str): The molecular structure given in SMILES format.
+            inh_pow (float): The inhibition power for the given molecule, this is the target
+                property.
+
+        Returns:
+            Data: A torch-geometric Data type which is a graph containing all the structural and
+                embedding information. If the SMILES string is unreadable or corrupt then None is
+                returned.
+        """
+        if self.molecule is None:
+            return None
+        
+        best_conf = self.find_best_conformer()
+
+        # Node features
+        x = torch.stack([self.atom_features(atom, best_conf) for atom in self.molecule.GetAtoms()])
+
+        # Edges
+        edge_index = []
+        edge_attr = []
+
+        for bond in self.molecule.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+
+            # Undirected graph → add both directions
+            edge_index.append([i, j])
+            edge_index.append([j, i])
+
+            bf = self.bond_features(bond)
+            edge_attr.append(bf)
+            edge_attr.append(bf)
+        
+        # Turns a list of tuples into a tensor and then changes the shape and memory type.
+        edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+        # Combines a list of tensors into one big tensor of the required shape.
+        edge_attr = torch.stack(edge_attr)
+
+        y = torch.tensor([self.inh_pow], dtype=torch.float)
+
+        return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
 
 
 def batch_from_csv(csv_path: str) -> list[Data]:
@@ -149,8 +211,9 @@ def batch_from_csv(csv_path: str) -> list[Data]:
     df = pd.read_csv(csv_path)
     graphs = []
     for _, row in df.iterrows():
-        g = mol_to_graph(row["SMILES"], row["Inh Power"])
-        if g is not None:
-            graphs.append(g)
+        g = MoleculeRepresentation(row["SMILES"], row["Inh Power"])
+        graph = g.mol_to_graph()
+        if graph is not None:
+            graphs.append(graph)
 
     return graphs
